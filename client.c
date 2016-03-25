c1adab0b45b9d90551f81e4b155dc6a7c3dc4cad
Aurèle <aurele.barriere@gmail.com>
2016-03-25 16:37:53 +0100

    first commit : what was done in TD

---
+++ client.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void error (char * msg) {
+  printf("\n error : ");
+  printf(msg);
+  printf("\n");
+  exit(1);
+}
+
+int main(int argc, char * argv[]) {
+  int sockfd, portno;
+  struct sockaddr_in serv_addr;
+  struct hostent * server;
+  int active = 1;
+  
+  char buffer[256];
+
+  // Checking port and address as argument
+  if (argc != 4) {
+    error("not 2 argument");
+  }
+
+  portno = atoi(argv[1]);
+
+  // checking host
+  if (server == NULL) { error(" no nuch server ");}
+
+
+
+  // Creating socket
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sockfd < 0) {error("creating socket");}
+
+ // Configure serv_addr
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[2]);
+  serv_addr.sin_port = htons(portno);
+
+  //connecting
+  if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    error(" connecting ");
+  }
+
+  // sending / recieving
+  while(active) {
+
+    memset(buffer, 0 , 256);
+    //fgets(buffer, 255, stdin);
+    
+    send(sockfd, argv[3], 256, 0);
+    recv(sockfd, buffer, 256, 0);
+    printf("What the server got : %s\n", buffer);
+    
+    active = 0; //exit after the msg
+  }
+  return 0;
+}
---
+++ server.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void error (char * msg) {
+  printf("\n error : ");
+  printf(msg);
+  printf("\n");
+  exit(1);
+}
+
+int main(int argc, char * argv[]) {
+  int sockfd, acksockfd, portno;
+  socklen_t clilen;
+  struct sockaddr_in cli_addr, serv_addr;
+  //struct hostent * server;
+  char buffer[256];
+  int active = 1;
+
+  // Checking port as argument
+  if (argc != 2) {
+    error("not 1 argument");
+  }
+
+  portno = atoi(argv[1]);
+
+
+  // Creating socket
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sockfd < 0) {error("creating socket");}
+
+  // Configure serv_addr
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  //memset ?
+
+
+  // binding socket
+  if (
+      bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
+      < 0) {
+    error("binding");
+  }
+  
+  // listening
+  listen(sockfd, 5);
+
+  // waiting for client to connect
+  while (active) {
+    acksockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    memset(buffer, 0, 256);
+    
+    recv(acksockfd, buffer, 256, 0);
+    printf("%s\n", buffer);
+    send(acksockfd, buffer, 256, 0);
+  }
+
+  
+ 
+  printf("\n Server shutdown \n");
+ return 0;
+}
+
+
